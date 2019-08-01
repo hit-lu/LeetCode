b7b105984c4282b488c00ab0212a38273bd866be
hit-lu <hit_lu@foxmail.com>
2019-08-01 19:01:57 +0800

    TwoSum

---
+++ Sum_of_Two_Numbers/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MaxTableSize = 10000;
+typedef enum {Legitimate, Empty, Deleted};
+typedef int ElementType;
+typedef int Index;
+
+class Solution{
+    private:
+
+    public:
+        vector<int> twoSum(vector<int>& nums, int target)
+        {
+
+        }
+};
+
+
+int main()
+{
+    //vector<int> a = {1,2,3,4};
+
+    return 0;
+}
