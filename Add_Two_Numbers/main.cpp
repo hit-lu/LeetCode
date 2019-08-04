0e4cadc6c704fffc40d650698dd97bfd34727ca3
hitlu1996 <hit_lu@foxmail.com>
2019-08-04 10:34:51 +0800

    finish the code of add two numbers

--- Add_Two_Numbers/main.cpp
+++ Add_Two_Numbers/main.cpp
@@ -1,61 +1,63 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
+
 using namespace std;
 struct ListNode
 {
-    int val;
-    ListNode* next;
-    ListNode(int x): val(x),next(NULL){};       // initialize
+	int val;
+	ListNode* next;
+	ListNode(int x) : val(x), next(NULL) {};       // initialize
 };
-class Soltion
+class Solution
 {
-    public:
-        ListNode* ConstuctList(vector<int> n)
-        {
-            ListNode* head = new ListNode(0), * cur = head;
-            for(int i = 0; i < n.size(); i ++)
-            {
-                cur = new ListNode(n[i]);
-                cur = cur->next;
-            }
-            return head->next;
+public:
+	ListNode* ConstuctList(vector<int> n)
+	{
+		ListNode* head = new ListNode(0), * cur = head;
+		for (int i = 0; i < n.size(); i++)
+		{
+			cur->next = new ListNode(n[i]);
+			cur = cur->next;
+		}
+		return head->next;
 
-        }
-        void PrintList(ListNode* head)
-        {
-            while(head->next != NULL)
-            {
-                cout << head->val << " -> ";
-                head = head->next;
-            }
-            cout << head->val << endl;
-        }
-        ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
-        {
-            ListNode* head = new ListNode(0);
-            ListNode* p = l1, * q = l2, * ret = head;
+	}
+	void PrintList(ListNode* head)
+	{
+		while (head->next != NULL)
+		{
+			cout << head->val << " -> ";
+			head = head->next;
+		}
+		cout << head->val << endl;
+	}
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+	{
+		ListNode* head = new ListNode(0);
+		ListNode* p = l1, *q = l2, *ret = head;
 
-            int v1, v2, sum, carry = 0;
-            while(p != NULL || q != NULL)
-            {
-                if(p != NULL)  v1 = p->val;    else  v1 = 0;
-                if(q != NULL)  v2 = q->val;    else  v2 = 0;
-                sum = v1 + v2 + carry;
-                carry = sum / 10;
+		int v1, v2, sum, carry = 0;
+		while (p != NULL || q != NULL)
+		{
+			if (p != NULL) 	v1 = p->val, p = p->next;    else  v1 = 0;
+			if (q != NULL)  v2 = q->val, q = q->next;    else  v2 = 0;
+			sum = v1 + v2 + carry;
+			carry = sum / 10;
 
-                ret->next = new ListNode(sum%10);
-                ret = ret->next;
-            }
-            return head->next;
-        }
+			ret->next = new ListNode(sum % 10);
+			ret = ret->next;
+		}
+		return head->next;
+	}
 };
 
 int main()
 {
-    Soltion S;
-    ListNode* l = S.ConstuctList({1,3,5,2});
-    S.PrintList(l);
+	Solution S;
+	ListNode* l1 = S.ConstuctList({ 0 });
+	ListNode* l2 = S.ConstuctList({ });
+	S.PrintList(S.addTwoNumbers(l1, l2));
 
-    cout << "end" << endl;
-    return 0;
+	cout << "end" << endl;
+	return 0;
 }
