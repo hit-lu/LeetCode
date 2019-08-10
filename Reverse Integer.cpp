33570452b7aaaa4b8439f354b1de67b321b2b03d
hitlu1996 <hit_lu@foxmail.com>
2019-08-10 21:23:12 +0800

    add an example of reverse integer

---
+++ Reverse Integer.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+class Solution 
+{
+	public:
+		int reverse(int x) 
+		{
+			int last_number, rev_number = 0;
+			while (x != 0)
+			{
+				last_number = x % 10;
+				x = x / 10;
+
+				if (rev_number == INT_MAX / 10 && last_number > 7 || rev_number > INT_MAX / 10)
+				{
+					rev_number = 0;
+					break;
+				}
+				else if(rev_number  == INT_MIN / 10 & last_number < -8 || rev_number < INT_MIN / 10)
+				{
+					rev_number = 0;
+					break;
+				}
+				rev_number = rev_number * 10 + last_number;
+			}
+			return rev_number;
+		}
+};
+int main()
+{
+	cout << Solution().reverse(12345) << endl;
+	return 0;
+}
